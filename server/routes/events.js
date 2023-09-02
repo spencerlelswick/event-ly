03f81874e61e48cff4308756e9552b3d7d2dd010
reddy <reddyfede@gmail.com>
2023-09-02 16:03:37 +0200

    add show,create,update,delete event routes

--- server/routes/events.js
+++ server/routes/events.js
@@ -4,5 +4,9 @@ const router = express.Router()
 const eventCtrl = require("../controllers/events")
 
 router.get("/", eventCtrl.index)
+router.post("/",eventCtrl.create)
+router.get("/:id", eventCtrl.show)
+router.put("/:id", eventCtrl.update)
+router.delete("/:id", eventCtrl.delete)
 
 module.exports = router
