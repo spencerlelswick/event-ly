f1d8d35ca2c5040f9ccc093a4fc4f19b451aca2d
SpencerlElswick <spencerlelswick@gmail.com>
2023-09-04 05:14:02 -0400

    Base map with locality stub

---
+++ client/src/mock/data.js
@@ -0,0 +1,61 @@
+
+
+ export const localityData = {
+  "authenticationResultCode": "ValidCredentials",
+  "brandLogoUri": "http://dev.virtualearth.net/Branding/logo_powered_by.png",
+  "copyright": "Copyright © 2023 Microsoft and its suppliers. All rights reserved. This API cannot be accessed and the content and any results may not be used, reproduced or transmitted in any manner without express written permission from Microsoft Corporation.",
+  "resourceSets": [
+      {
+          "estimatedTotal": 1,
+          "resources": [
+              {
+                  "__type": "Location:http://schemas.microsoft.com/search/local/ws/rest/v1",
+                  "bbox": [
+                      38.26502728242932,
+                      -85.49390658782691,
+                      38.272752717570675,
+                      -85.48078741217309
+                  ],
+                  "name": "2301 Cleary Ct, Louisville, KY 40245, United States",
+                  "point": {
+                      "type": "Point",
+                      "coordinates": [
+                          38.26889,
+                          -85.487347
+                      ]
+                  },
+                  "address": {
+                      "addressLine": "2301 Cleary Ct",
+                      "adminDistrict": "KY",
+                      "adminDistrict2": "Jefferson Co.",
+                      "countryRegion": "United States",
+                      "formattedAddress": "2301 Cleary Ct, Louisville, KY 40245, United States",
+                      "locality": "Louisville",
+                      "postalCode": "40245"
+                  },
+                  "confidence": "High",
+                  "entityType": "Address",
+                  "geocodePoints": [
+                      {
+                          "type": "Point",
+                          "coordinates": [
+                              38.26889,
+                              -85.487347
+                          ],
+                          "calculationMethod": "Parcel",
+                          "usageTypes": [
+                              "Display"
+                          ]
+                      }
+                  ],
+                  "matchCodes": [
+                      "Good"
+                  ]
+              }
+          ]
+      }
+  ],
+  "statusCode": 200,
+  "statusDescription": "OK",
+  "traceId": "fca12d891cf149c6acbc89338a6095bf|BN00006C46|0.0.0.1"
+}
