eab4a7c9d1a00880925b46aa628545c82587c8d0
SpencerlElswick <spencerlelswick@gmail.com>
2023-09-04 06:36:54 -0400

    Adds add events button

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,13 +3,26 @@ import Map from '../components/Map';
 import Events from '../components/Events';
 
 const Home = () => {
+
+  const handleAddEvent = () => {
+    
+  }
+
   return (
     <div className='sm:text-2xl h-full'>
       <div
         style={{ height: '95vh' }}
         className='z-10 invisible sm:visible absolute bg-yellow-600 right-0 w-2/5 overflow-auto'
       >
-        Event List:
+        <div className='flex justify-between m-2'>
+          <h2 className='text-4xl'>Event List:</h2>
+          <button
+            onClick={handleAddEvent}
+            className='border-solid border-black border-2'
+          >
+            Add Event
+          </button>
+        </div>
         <Events />
       </div>
       <div className='z-10 sm:invisible absolute bg-yellow-400 right-10 bottom-10 h-20 w-20 rounded-full flex justify-center items-center'>
