a36b026f600259149b35a9ef36a96644315ce5eb
reddy <reddyfede@gmail.com>
2023-09-06 18:01:56 +0200

    add auth0

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import LoginButton from './Auth/LoginButton';
+import LogoutButton from './Auth/LogoutButton';
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Header = () => {
+
+  const { user, isAuthenticated, isLoading } = useAuth0()
+  console.log(user)
   return (
     <header style={{ height: '5vh' }} className='bg-primary text-white'>
       <div id='mobile-menu' className='sm:hidden flex justify-between'>
         <p>search</p>
         <h1>Logo</h1>
-        <p>menu</p>
+        {!isLoading ? (
+          isAuthenticated ? (
+            <LogoutButton/>
+          ) : (
+            <LoginButton/>
+          )
+        ) : (null)}
       </div>
       <div id='desktop-menu' className='hidden sm:flex justify-between'>
         <h1>Logo</h1>
@@ -15,7 +27,15 @@ const Header = () => {
           <li className='mx-2'>link 2</li>
           <li className='mx-2'>link 3</li>
         </ul>
-        <p>menu</p>
+
+        {!isLoading ? (
+          isAuthenticated ? (
+            <LogoutButton/>
+          ) : (
+            <LoginButton/>
+          )
+        ) : (null)}
+
       </div>
     </header>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createRoot } from "react-dom/client";
+import Auth0ProviderWithNavigate from './components/Auth/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Auth0ProviderWithNavigate>
+        <App />
+      </Auth0ProviderWithNavigate>
     </Router>
   </React.StrictMode>
 );
