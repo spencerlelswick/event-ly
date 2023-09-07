0e7a737c3b41fe4f0752835491d459cbbd33e6a8
SpencerlElswick <spencerlelswick@gmail.com>
2023-09-07 18:31:00 -0400

    Adds custom pins and pin icons

--- client/src/components/MapPin.jsx
+++ client/src/components/MapPin.jsx
@@ -1,14 +1,33 @@
-import { Marker,Popup } from "react-leaflet"
-import EventDetailsModal from "./EventDetailsModal"
+import { Marker, Popup } from 'react-leaflet';
+import EventDetailsModal from './EventDetailsModal';
 
-export default function MapPin({event}){
-    return(
-        <Marker position={[event.coordinates.latitude, event.coordinates.longitude]}>
-            <Popup>
-                <img src={event.image} alt={event.name}/>
-                <div>{event.name} <br /> {event.address} </div>
-                <EventDetailsModal modalId={event._id+"2"} eventId={event._id}/>
-            </Popup>
-        </Marker>
-    )
+export default function MapPin({ event }) {
+  console.log(`./src/assets/${event.category}.svg`);
+
+  const pointerIcon = new L.Icon({
+    iconUrl: `./src/assets/${event.category}.svg`,
+    // iconRetinaUrl: `./src/assets/${event.category}.svg`,
+    iconRetinaUrl: `./src/assets/${event.category}.svg`,
+    iconAnchor: [8, 36],
+    popupAnchor: [10, -44],
+    iconSize: [16, 16],
+    shadowUrl: `./src/assets/marker-bg-${event.category}.png`,
+    shadowSize: [46, 46],
+    shadowAnchor: [22, 44],
+  });
+
+  return (
+    <Marker
+      position={[event.coordinates.latitude, event.coordinates.longitude]}
+      icon={pointerIcon}
+    >
+      <Popup>
+        <img src={event.image} alt={event.name} />
+        <div>
+          {event.name} <br /> {event.address}{' '}
+        </div>
+        <EventDetailsModal modalId={event._id + '2'} eventId={event._id} />
+      </Popup>
+    </Marker>
+  );
 }
