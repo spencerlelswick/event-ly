8a7c61aa196456503ffe768675afff8d9c5a9dd6
reddy <reddyfede@gmail.com>
2023-09-07 23:04:30 +0200

    create user panel

--- client/src/pages/UserPanel.jsx
+++ client/src/pages/UserPanel.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { showUser } from '../utilities/users-service';
+import { useContext, useState } from 'react';
+import { UserContext } from '../components/App';
 
-const UserPanel = () => {
-  return <div>User Panel</div>;
+export default function UserPanel() {
+  const currUser = useContext(UserContext)
+  const [userInfo, setUserInfo] = useState(null)
+
+  async function retrieveUser(){
+    try{
+      if (currUser){
+      const data = await showUser(currUser)
+      console.log("data",data)
+      setUserInfo(data)
+      console.log(userInfo)
+      }
+    }catch (err){
+      console.log(err)
+    }
+  }
+
+  useEffect(()=>{
+    retrieveUser()
+  },[currUser])
+
+  return (
+    <div>
+
+      <h1>USER PANEL</h1>
+      {userInfo ? (
+        <div>{userInfo.name} </div>
+      ):(null)}
+
+
+
+    </div>
+  );
 };
 
-export default UserPanel;
+
