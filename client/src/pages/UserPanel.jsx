e38b74e39b1f4cbeb9935ae4e29f19b56f86bb7e
reddy <reddyfede@gmail.com>
2023-09-08 11:07:54 +0200

    add restrictions to other people panel

--- client/src/pages/UserPanel.jsx
+++ client/src/pages/UserPanel.jsx
@@ -1,28 +1,53 @@
 import React, { useEffect } from 'react';
-import { showUser } from '../utilities/users-service';
 import { useContext, useState } from 'react';
 import { UserContext } from '../components/App';
 import { getAllEvents } from '../utilities/events-service';
 import { useParams } from "react-router-dom";
+import { showUser } from '../utilities/users-service';
+import { useAuth0 } from "@auth0/auth0-react"
 
 export default function UserPanel() {
+  const {isLoading } = useAuth0()
   const currUser = useContext(UserContext)
-  const routeParams = useParams()
+  const routeId = useParams().id
+  const [routeUser, setRouteUser] = useState(null)
   const [createdEvents, setCreatedEvents] = useState(null)
   const [attendingEvents, setAttendingEvents] = useState(null)
   const [loadingEvents, setLoadingEvents] = useState(true)
+  const [loadingUser, setLoadingUser] = useState(true)
 
+  async function retrieveUser() {
+    try {
+      if (currUser) {
+        if (currUser.ID === routeId) {
+          setRouteUser({
+            name: currUser.NAME,
+            picture: currUser.PIC,
+            _id: currUser.ID
+          })
+          setLoadingUser(false)
+        } else {
+          const res = await showUser(routeId)
+          if (res._id) {
+            setRouteUser(res)
+            setLoadingUser(false)
+          }
+        }
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   async function retrieveEvents() {
     try {
-      if (currUser) {
-        const userId = routeParams.id
-        const allEvents = await getAllEvents({ userId: userId, filterBy: "user" })
+      if (currUser && routeUser) {
+        const allEvents = await getAllEvents({ userId: routeId, filterBy: "user" })
         if (allEvents.length >= 0) {
           const created = []
           const attending = []
           allEvents.map((event) => (
-            event.createdBy === userId ? created.push(event) : attending.push(event)
+            event.createdBy === routeId ? created.push(event) : attending.push(event)
           ))
           setCreatedEvents(created)
           setAttendingEvents(attending)
@@ -34,60 +59,115 @@ export default function UserPanel() {
     }
   }
 
+  useEffect(() => {
+    retrieveUser()
+  }, [currUser, routeId])
 
   useEffect(() => {
     retrieveEvents()
-  }, [currUser])
+  }, [routeUser])
+
+  async function handleRemove(e){
+    try{
+        e.preventDefault()
+        const data = [...event.guests]
+        const idx = data.indexOf(data.find((g)=>(g._id === currUser.ID)))
+        data.splice(idx,1)
+        const updatedEvent = await updateEvent(event._id, {guests: data})
+        if (updatedEvent._id){
+            setEvent(updatedEvent)
+        }else {
+            throw Error("Something went wrong with removing a guest.")
+        }
+    }catch(err){    
+        console.log(err)
+    }
+}
+
+
+
 
   return (
     <div>
       <h1>USER PANEL</h1>
-      {currUser ? (
-        <>
-          <img src={currUser.PIC} />
-          <div>{currUser.NAME}</div>
-          <hr />
+      {!isLoading ? (<>
+        {currUser ? (
+          <>
+            {loadingUser ? (
+              <div>Loading User Info...</div>
+            ) : (
+              <div>
+                <img src={routeUser.picture} alt={routeUser.name} className='w-20'/>
+                <div>{routeUser.name}</div>
+              </div>
+            )}
 
+            <hr />
 
-          {loadingEvents ? (
-            <div>Loading Created Events...</div>
-          ) : (
-            <>
-              <h1>CREATED EVENTS</h1>
-              {createdEvents.length ? (
-                <>
-                  {createdEvents.map((event) => (
-                    <div key={event._id}>
-                      <div>{event.name}</div>
-                      <img src={event.image} alt={event.name} className=' w-20' />
-                      {console.log(event)}
-                    </div>
-                  ))}
-                </>
-              ) : (
-                <div>You don't have any created event. Plan something!</div>
-              )}
+            {loadingEvents ? (
+              <div>Loading Events...</div>
+            ) : (
+              <>
+                <h1>CREATED EVENTS</h1>
+                {createdEvents.length ? (
+                  <>
+                    {createdEvents.map((event) => (
+                      <div key={event._id}>
+                        <br />
+                        <div>{event.name}</div>
+                        <img src={event.image} alt={event.name} className=' w-20' />
+                        <div>Description: {event.description}</div>
+                        <div>Address: {event.address}</div>
+                        <div>Location: {event.location}</div>
+                        <div>Date: {event.date}</div>
+                        <div>Category: {event.category}</div>
+                        {currUser.ID === routeId ? (
+                          <button className='btn btn-secondary'>Delete event</button>
+                        ) : (
+                          null
+                        )}
+                      </div>
+                    ))}
+                  </>
+                ) : (
+                  <div>Not hosting any event yet.</div>
+                )}
 
-              <hr />
-              <h1>ATTENDING EVENTS</h1>
+                <hr />
+                <h1>ATTENDING EVENTS</h1>
                 {attendingEvents.length ? (
                   <>
                     {attendingEvents.map((event) => (
                       <div key={event._id}>
                         <div>{event.name}</div>
                         <img src={event.image} alt={event.name} className=' w-20' />
+                        <div>Description: {event.description}</div>
+                        <div>Address: {event.address}</div>
+                        <div>Location: {event.location}</div>
+                        <div>Date: {event.date}</div>
+                        <div>Category: {event.category}</div>
+                        {currUser.ID === routeId ? (
+                          <button className='btn btn-secondary'>Remove me</button>
+                        ) : (
+                          null
+                        )}
                       </div>
                     ))}
                   </>
                 ) : (
-                  <div>Seems like you are not partecipating at any events. Go explore events in your area!</div>
+                  <div>Not attending any event yet.</div>
                 )}
-            </>
-          )}
-        </>
-      ) : (
-        <div>Loading Content...</div>
-      )}
+
+              </>
+            )}
+          </>
+        ) : (
+          <div>Log in to see info</div>
+        )}
+      </>
+      ) :
+        <>Loading Content...</>
+      }
     </div>
   );
 };
