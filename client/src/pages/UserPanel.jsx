20283e2deb007748b986d29afc612d8b2eb31fca
reddy <reddyfede@gmail.com>
2023-09-10 15:44:22 +0200

    add tabs to user profile

--- client/src/pages/UserPanel.jsx
+++ client/src/pages/UserPanel.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import { useContext, useState } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { UserContext } from '../components/App';
 import { getAllEvents } from '../utilities/events-service';
 import { useParams } from "react-router-dom";
@@ -9,7 +8,7 @@ import UserPanelAttendingItem from '../components/UserPanelAttendingItem';
 import UserPanelCreatedItem from '../components/UserPanelCreatedItem';
 
 export default function UserPanel() {
-  const {isLoading } = useAuth0()
+  const { isLoading } = useAuth0()
   const currUser = useContext(UserContext)
   const routeId = useParams().id
   const [routeUser, setRouteUser] = useState(null)
@@ -17,6 +16,7 @@ export default function UserPanel() {
   const [attendingEvents, setAttendingEvents] = useState(null)
   const [loadingEvents, setLoadingEvents] = useState(true)
   const [loadingUser, setLoadingUser] = useState(true)
+  const [activeTab, setActiveTab] = useState(1)
 
   async function retrieveUser() {
     try {
@@ -73,8 +73,17 @@ export default function UserPanel() {
     retrieveEvents()
   }, [routeUser])
 
+  function handleClick(e) {
+    e.preventDefault()
+    console.log(e.target.id, e.target.className)
+    setActiveTab(e.target.id)
+  }
+
+  const active = "tab tab-lifted tab-active"
+  const inactive = "tab tab-lifted"
+
   return (
-    <div>
+    <div className="flex flex-col justify-center align-middle items-center w-full">
       <h1>USER PANEL</h1>
       {!isLoading ? (<>
         {currUser ? (
@@ -83,47 +92,60 @@ export default function UserPanel() {
               <div>Loading User Info...</div>
             ) : (
               <div>
-                <img src={routeUser.picture} alt={routeUser.name} className='w-20'/>
+                <img src={routeUser.picture} alt={routeUser.name} className='w-30 rounded-xl' />
                 <div>{routeUser.name}</div>
               </div>
             )}
 
-            <hr />
+            <br />
+
+            <div className="tabs">
+              <a id={1} className={activeTab == 1 ? active : inactive} onClick={handleClick} >
+                CREATED EVENTS
+              </a>
+              <a id={2} className={activeTab == 2 ? active : inactive} onClick={handleClick} >
+                ATTENDING EVENTS
+              </a>
+            </div>
+
+            <br/>
 
             {loadingEvents ? (
               <div>Loading Events...</div>
             ) : (
-              <>
-                <h1>CREATED EVENTS</h1>
-                {createdEvents.length ? (
-                  <>
-                    {createdEvents.map((event) => (
-                       <UserPanelCreatedItem key={event._id} event={event} currUser={currUser} routeId={routeId}
-                       createdEvents={createdEvents} setCreatedEvents={setCreatedEvents}
-                       retrieveEvents={retrieveEvents}
-                       />
-                    ))}
-                  </>
-                ) : (
-                  <div>Not hosting any event yet.</div>
-                )}
+              <div>
 
-                <hr />
-                <h1>ATTENDING EVENTS</h1>
-                {attendingEvents.length ? (
-                  <>
-                    {attendingEvents.map((event) => (
-                      <UserPanelAttendingItem key={event._id} event={event} currUser={currUser} routeId={routeId}
-                      attendingEvents={attendingEvents} setAttendingEvents={setAttendingEvents}
-                      retrieveEvents={retrieveEvents}
-                      />
-                    ))}
-                  </>
-                ) : (
-                  <div>Not attending any event yet.</div>
-                )}
+                <section hidden={activeTab == 2}>
+                  {createdEvents.length ? (
+                    <>
+                      {createdEvents.map((event) => (
+                        <UserPanelCreatedItem key={event._id} event={event} currUser={currUser} routeId={routeId}
+                          createdEvents={createdEvents} setCreatedEvents={setCreatedEvents}
+                          retrieveEvents={retrieveEvents}
+                        />
+                      ))}
+                    </>
+                  ) : (
+                    <div>Not hosting any event yet.</div>
+                  )}
+                </section>
 
-              </>
+                <section hidden={activeTab == 1}>
+                  {attendingEvents.length ? (
+                    <>
+                      {attendingEvents.map((event) => (
+                        <UserPanelAttendingItem key={event._id} event={event} currUser={currUser} routeId={routeId}
+                          attendingEvents={attendingEvents} setAttendingEvents={setAttendingEvents}
+                          retrieveEvents={retrieveEvents}
+                        />
+                      ))}
+                    </>
+                  ) : (
+                    <div>Not attending any event yet.</div>
+                  )}
+                </section>
+
+              </div>
             )}
           </>
         ) : (
