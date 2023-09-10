92541db9b60aa7ca942d118563e1b4f8815d4350
SpencerlElswick <spencerlelswick@gmail.com>
2023-09-10 01:59:35 -0400

    Styles list toggle button

--- client/src/components/EventsListCollapsed.jsx
+++ client/src/components/EventsListCollapsed.jsx
@@ -1,7 +1,9 @@
+import { FaListOl } from 'react-icons/fa';
+
 export default function EventsListCollapsed() {
   return (
-    <div className='z-[1] sm:invisible absolute bg-yellow-400 right-10 top-20 h-20 w-20 rounded-full flex justify-center items-center'>
-      list
+    <div className='z-[1] sm:invisible absolute btn btn-secondary text-white text-4xl right-10 top-20 h-20 w-20 rounded-full flex justify-center items-center'>
+      <FaListOl />
     </div>
   );
 }
