473c2c12df6a95b190498c7c2f42dfcb33c6bf49
SpencerlElswick <spencerlelswick@gmail.com>
2023-09-11 05:58:23 -0400

    Style sort to full width on mobile

--- client/src/components/EventsListSort.jsx
+++ client/src/components/EventsListSort.jsx
@@ -1,19 +1,19 @@
-export default function EventsListSort({setSorted}) {
-    return(
-        <div className='flex justify-between m-2'>
-        <form >
-          <select
-          defaultValue={"date"}
-          className='select select-bordered select-primary'
-          onChange={(e)=>setSorted(e.target.value)}
-          >
-            <option value={"date"}>Upcoming</option>
-            <option value={"dist"}>Proximity</option>
-            <option value={"guests"}>Number of guests</option>
-            <option value={"name"}>Name - ascending</option>
-            <option value={"nameInv"}>Name - descending</option>
-          </select>
-        </form>
+export default function EventsListSort({ setSorted }) {
+  return (
+    <form>
+      <div className='flex justify-between m-2'>
+        <select
+          defaultValue={'date'}
+          className='select select-bordered select-primary w-full md:w-1/2'
+          onChange={(e) => setSorted(e.target.value)}
+        >
+          <option value={'date'}>Upcoming</option>
+          <option value={'dist'}>Proximity</option>
+          <option value={'guests'}>Number of guests</option>
+          <option value={'name'}>Name - ascending</option>
+          <option value={'nameInv'}>Name - descending</option>
+        </select>
       </div>
-    )
+    </form>
+  );
 }
