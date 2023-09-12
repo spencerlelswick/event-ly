8aa76943088dc2da42da496908bd8135f2c0869a
reddy <reddyfede@gmail.com>
2023-09-12 15:02:13 +0200

    after login show the user pic and the logout btn

--- client/src/components/LogoutButton.jsx
+++ client/src/components/LogoutButton.jsx
@@ -6,21 +6,27 @@ const LogoutButton = ({ currUser }) => {
   const { logout } = useAuth0();
 
   return (
-    <button
-      onClick={() =>
-        logout({ logoutParams: { returnTo: window.location.origin } })
-      }
-      className='text-white text-3xl sm:text-2xl w-1/3 flex items-center justify-end'
-    >
+    <div className='flex justify-center items-center'>
+
       {currUser?.PIC ? (
-        <div className='flex justify-center items-center'>
+        <>
           <span className='mr-2 text-sm'>{currUser.NAME.split(' ')[0]}</span>
           <img src={currUser.PIC} alt='avatar' className='btn-circle p-1' />
-        </div>
+        </>
       ) : (
-        <MdLogout />
+        null
       )}
-    </button>
+
+      <button
+        onClick={() =>
+          logout({ logoutParams: { returnTo: window.location.origin } })
+        }
+        className='text-white text-3xl sm:text-2xl w-1/3 flex items-center justify-end'
+      >
+        <MdLogout />
+      </button>
+
+    </div>
   );
 };
 
