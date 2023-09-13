55a1821ab98e0d2ba7e6a3249a6ae1a5781b0372
SpencerlElswick <spencerlelswick@gmail.com>
2023-09-13 02:22:28 -0400

    resize nav icon and add user tooltip

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,15 +5,13 @@ import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createRoot } from "react-dom/client";
+import { createRoot } from 'react-dom/client';
 import Auth0ProviderWithNavigate from './components/Auth/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <Auth0ProviderWithNavigate>
-        <App />
-      </Auth0ProviderWithNavigate>
-    </Router>
-  </React.StrictMode>
+  <Router>
+    <Auth0ProviderWithNavigate>
+      <App />
+    </Auth0ProviderWithNavigate>
+  </Router>
 );
